7890fd142f92e9800b79815cd7e1932f32bdbb9d
CyberEmir <32274017+Taofeeq90@users.noreply.github.com>
2021-08-19 11:11:21 -0500

    Add files via upload

---
+++ FIN_Scan1.py
@@ -0,0 +1,6 @@
+from scapy.all import * 
+ip1 = IP(src="192.168.0.10", dst ="192.168.0.3" )
+tcp1 = TCP(sport =1024, dport=80, flags="F", seq=12345)
+packet = ip1/tcp1 
+p =sr1(packet, inter=1) 
+p.show()
